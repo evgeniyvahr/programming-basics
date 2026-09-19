4f6e58595c38df57f8af18194ec9c8ec49a89882
evgeniyvahr <150697903+evgeniyvahr@users.noreply.github.com>
2026-09-19 14:03:03 +0300

    Create task_3.py

---
+++ lab_01/task_3.py
@@ -0,0 +1,34 @@
+import math
+# Эксперимент A
+# x = 17
+# y = 5
+# print(x + y, type(x + y))
+# print(x - y, type(x - y))
+# print(x * y, type(x * y))
+# print(x / y, type(x / y))
+# print(x // y, type(x // y))
+# print(x % y, type(x % y))
+# print(x ** y, type(x ** y))
+
+# Эксперимент B
+# print(2 ** 1000)
+
+# Эксперимент C
+# result = 0.1 + 0.2
+# print(result)
+# print(result == 0.3)
+# print(result - 0.3)
+# print(math.isclose(result, 0.3))
+
+# Эксперимент D
+print(int("42"), type(int("42")))
+print(float(3.14), type(float(3.14)))
+print(str(2026), type(str(2026)))
+print(bool(0), type(bool(0)))
+print(bool(-1), type(bool(-1)))
+print(bool(""), type(bool("")))
+print(bool("False"), type(bool("False")))
+print(complex(2, -3), type(complex(2, -3)))
+x = complex(2, -3)
+print(x.real)
+print(x.imag)
