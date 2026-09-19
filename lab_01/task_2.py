ebc65dbbb186bafd195021d3bbc287d76b9e1e7f
evgeniyvahr <150697903+evgeniyvahr@users.noreply.github.com>
2026-09-19 13:25:54 +0300

    Create task_2.py

---
+++ lab_01/task_2.py
@@ -0,0 +1,17 @@
+# a = 1000
+# b = a
+# c = None
+#
+# print("Типы:", type(a), type(b), type(c))
+# print("Идентификаторы:", id(a), id(b), id(c))
+# print("a == b:", a == b)
+# print("a is b:", a is b)
+# print("a == c:", a == c)
+# print("a is c:", a is c)
+# print("c is None", c is None)
+
+first = "python"
+second = "py" + "thon"
+
+print(first == second)
+print(first is second)
